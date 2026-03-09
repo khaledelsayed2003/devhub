f10dfc8bf1dc1c867c984888e683187b3394880c
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2026-03-09 23:51:17 +0300

    refactor: move templates into their respective app directories

--- projects/views.py
+++ projects/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    return render(request, 'projects.html')
+    return render(request, 'projects/projects.html')
 
 def project(request):
-    return render(request, 'single-project.html')
+    return render(request, 'projects/single-project.html')
