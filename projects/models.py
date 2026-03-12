3a4624c61b402f07343fc8767264e9c9eba8b094
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2026-03-12 23:59:02 +0300

    feat: add Project model with UUID, title, description, links, and timestamp and generate initial migration

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Project(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    title = models.CharField(max_length=250)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
