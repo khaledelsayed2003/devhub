9c9bee603c3cc023f5e1bb236dcc6c8e112fd023
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2026-03-12 19:34:19 +0300

    feat: render project dynamically using pk from mock projects list

--- projects/views.py
+++ projects/views.py
@@ -2,8 +2,41 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def projects(request):
-    return render(request, 'projects/projects.html')
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'A fully functional ecommerce website with cart and payment integration'
+    },
+    {
+        'id': '2',
+        'title': 'DevHub',
+        'description': 'A developer portfolio and community platform built with Django'
+    },
+    {
+        'id': '3',
+        'title': 'Weather App',
+        'description': 'A weather forecast app that fetches real time data from an API'
+    },
+    {
+        'id': '4',
+        'title': 'Chat Application',
+        'description': 'A real time chat app with private messaging and group rooms'
+    },
+    {
+        'id': '5',
+        'title': 'Task Manager',
+        'description': 'A productivity app to manage daily tasks with priorities and deadlines'
+    },
+]
 
-def project(request):
-    return render(request, 'projects/single-project.html')
+
+def projects(request):
+    return render(request, 'projects/projects.html', {'projects': projectsList})
+
+def project(request, pk):
+    projectObj = None
+    for item in projectsList:
+        if item['id'] == pk:
+            projectObj = item
+    return render(request, 'projects/single-project.html', {'project': projectObj})
