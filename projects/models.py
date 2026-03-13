775fb0ac25aacf5eab8609140a7c35635d0e927a
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2026-03-13 21:52:35 +0300

    feat: add Tag model and link it to Project via ManyToMany, add vote fields

--- projects/models.py
+++ projects/models.py
@@ -7,6 +7,9 @@ class Project(models.Model):
     description = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
@@ -28,3 +31,11 @@ class Review(models.Model):
     def __str__(self):
         return self.value
     
+
+class Tag(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    name = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
