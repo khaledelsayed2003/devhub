f627c182b2b83e06373a3155d7cca48e2fb5ba9d
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2026-03-14 19:16:03 +0300

    feat: replace dummy data with real database queries in projects views

--- projects/views.py
+++ projects/views.py
@@ -1,42 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
 
-
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'A fully functional ecommerce website with cart and payment integration'
-    },
-    {
-        'id': '2',
-        'title': 'DevHub',
-        'description': 'A developer portfolio and community platform built with Django'
-    },
-    {
-        'id': '3',
-        'title': 'Weather App',
-        'description': 'A weather forecast app that fetches real time data from an API'
-    },
-    {
-        'id': '4',
-        'title': 'Chat Application',
-        'description': 'A real time chat app with private messaging and group rooms'
-    },
-    {
-        'id': '5',
-        'title': 'Task Manager',
-        'description': 'A productivity app to manage daily tasks with priorities and deadlines'
-    },
-]
 
 
 def projects(request):
+    projectsList = Project.objects.all()
     return render(request, 'projects/projects.html', {'projects': projectsList})
 
 def project(request, pk):
-    projectObj = None
-    for item in projectsList:
-        if item['id'] == pk:
-            projectObj = item
+    projectObj = Project.objects.get(id=pk)
     return render(request, 'projects/single-project.html', {'project': projectObj})
