929573cc28d00ee25c0e93433b409daf89e1d654
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2026-03-27 22:36:50 +0300

    Add featured image column to project model with default image support

--- projects/models.py
+++ projects/models.py
@@ -5,6 +5,7 @@ class Project(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     title = models.CharField(max_length=250)
     description = models.TextField(null=True, blank=True)
+    featured_image = models.ImageField(null=True, blank=True, default="default.png")
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
     tags = models.ManyToManyField('Tag', blank=True)
