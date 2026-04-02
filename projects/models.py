11457c73b67fc6078fa3634be55ead93a27341df
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2026-04-02 19:46:19 +0300

    feat(models): add username field to Profile and connect Project to Profile via ForeignKey

--- projects/models.py
+++ projects/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 import uuid
+from users.models import Profile
 
 class Project(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    owner = models.ForeignKey(Profile, on_delete=models.SET_NULL)
     title = models.CharField(max_length=250)
     description = models.TextField(null=True, blank=True)
     featured_image = models.ImageField(null=True, blank=True, default="default.png")
--- users/models.py
+++ users/models.py
@@ -7,6 +7,7 @@ class Profile(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=150, null=True, blank=True)
+    username = models.CharField(max_length=200, null=True, blank=True)
     email = models.EmailField(max_length=350, null=True, blank=True)
     short_intro = models.CharField(max_length=250, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
