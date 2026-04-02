3342c96877fa18ec7273993295d39f27c232c9ba
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2026-04-02 19:55:33 +0300

    fix(models): allow null owner and apply migrations

--- projects/models.py
+++ projects/models.py
@@ -4,7 +4,7 @@ from users.models import Profile
 
 class Project(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    owner = models.ForeignKey(Profile, on_delete=models.SET_NULL)
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=250)
     description = models.TextField(null=True, blank=True)
     featured_image = models.ImageField(null=True, blank=True, default="default.png")
