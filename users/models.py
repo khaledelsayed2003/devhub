85035ac49c09519ab33bbcb35db74076d5285904
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2026-04-02 19:27:03 +0300

    feat(users): implement Profile model with admin registration and database migrations

--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
+class Profile(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=150, null=True, blank=True)
+    email = models.EmailField(max_length=350, null=True, blank=True)
+    short_intro = models.CharField(max_length=250, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    profile_image = models.ImageField(null=True, blank=True, upload_to='profiles/', default='profiles/default.png')
+    social_linkedin = models.CharField(max_length=250, null=True, blank=True)
+    social_github = models.CharField(max_length=250, null=True, blank=True)
+    social_insta = models.CharField(max_length=250, null=True, blank=True)
+    social_youtube = models.CharField(max_length=250, null=True, blank=True)
+    social_website = models.CharField(max_length=250, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.user.username)
