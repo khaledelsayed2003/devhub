717990d8d32cf5b09ac09d74bc2dc96c0a0707d3
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2026-04-02 23:40:38 +0300

    Add Skill model, register it in admin, and extend profiles with location

--- users/models.py
+++ users/models.py
@@ -9,6 +9,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=150, null=True, blank=True)
     username = models.CharField(max_length=200, null=True, blank=True)
     email = models.EmailField(max_length=350, null=True, blank=True)
+    location = models.CharField(max_length=350, null=True, blank=True)
     short_intro = models.CharField(max_length=250, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     profile_image = models.ImageField(null=True, blank=True, upload_to='profiles/', default='profiles/default.png')
@@ -21,3 +22,15 @@ class Profile(models.Model):
     
     def __str__(self):
         return str(self.user.username)
+    
+    
+
+class Skill(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=250, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.user.name)
