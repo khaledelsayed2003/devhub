002cbfdd78197b7e7beb8cff91da65685f2f12b6
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2026-04-03 23:30:13 +0300

    Add developer profile page and link profile cards to it

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.db.models import Prefetch, Q
+from django.shortcuts import get_object_or_404, render
 from .models import Profile
+from projects.models import Project
 
 # Create your views here.
 
@@ -8,15 +10,23 @@ def profiles(request):
     return render(request, 'users/profiles.html', {'developers': users})
 
 def userProfile(request, pk):
-    profile = Profile.objects.get(id=pk)
+    profile = get_object_or_404(
+        Profile.objects.prefetch_related(
+            'skill_set',
+            Prefetch('project_set', queryset=Project.objects.prefetch_related('tags').order_by('-created')),
+        ),
+        id=pk,
+    )
 
-    topSkills = profile.skill_set.exclude(description__exact="")
-    otherSkills = profile.skill_set.filter(description="")
+    topSkills = profile.skill_set.exclude(description__isnull=True).exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(Q(description__isnull=True) | Q(description=""))
+    projects = profile.project_set.all()
 
     context = {
         'profile': profile,
         'topSkills': topSkills,
-        'otherSkills': otherSkills
+        'otherSkills': otherSkills,
+        'projects': projects,
     }
 
     return render(request, 'users/user-profile.html', context)
