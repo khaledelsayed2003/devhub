5003d90641126fd1e7efff9e729f893c84cd38a4
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2026-04-03 22:54:45 +0300

    feat(users): implement single profile view with URL and basic template (structure pending)

--- users/views.py
+++ users/views.py
@@ -6,3 +6,17 @@ from .models import Profile
 def profiles(request):
     users = Profile.objects.all()
     return render(request, 'users/profiles.html', {'developers': users})
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(description="")
+
+    context = {
+        'profile': profile,
+        'topSkills': topSkills,
+        'otherSkills': otherSkills
+    }
+
+    return render(request, 'users/user-profile.html', context)
