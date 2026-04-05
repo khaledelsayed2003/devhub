c28239eda3bdbfdcf251469289a7aaff897255f5
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2026-04-05 22:52:35 +0300

    refactor(users): move profile signals to signals.py and register them in app config

--- users/models.py
+++ users/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
-from django.db.models.signals import post_save, post_delete
-from django.dispatch import receiver
-
 
 # Create your models here.
 class Profile(models.Model):
@@ -39,22 +36,3 @@ class Skill(models.Model):
         return str(self.name)
     
     
-
-def createProfile(sender, instance, created, **kwargs):
-    if created:
-        user = instance
-        profile = Profile.objects.create(
-            user=user,
-            username=user.username,
-            email=user.email,
-            name=user.first_name,
-        )
-
-
-def deleteUser(sender, instance, **kwargs):
-    user = instance.user
-    user.delete()
-
-
-post_save.connect(createProfile, sender=User)
-post_delete.connect(deleteUser, sender=Profile)
