53c1e6a564f14a754a9ab5ab910d0499c4699723
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2026-04-14 23:59:57 +0300

    Expand project search to include tag names

--- projects/views.py
+++ projects/views.py
@@ -13,10 +13,12 @@ def projects(request):
     
     if request.GET.get('search_query_project'):
         search_query = request.GET.get('search_query_project')
+        
+    tags = Tag.objects.filter(name__icontains=search_query)
     
-    projects_list = Project.objects.prefetch_related('tags').filter(
+    projects_list = Project.objects.prefetch_related('tags').distinct().filter(
         Q(title__icontains=search_query) | Q(description__icontains=search_query) |
-        Q(owner__name__icontains=search_query)).order_by('-created')
+        Q(owner__name__icontains=search_query) | Q(tags__in=tags)).order_by('-created')
     featured_project = (
         Project.objects.prefetch_related('tags')
         .order_by('-vote_total', '-vote_ratio', '-created')
